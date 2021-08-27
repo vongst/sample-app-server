d9bd7deb6abb80a69ae5cfbcaf0aca07272b6907
vongst <st@vong.io>
2021-08-27 11:39:07 +0800

    http server

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -18,16 +26,63 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+
+	jsonFilePath, _ := filepath.Abs("./data/data_6_out.json")
+
+	type Customer struct {
+		ID          int               `json:"id"`
+		Attributes  map[string]string `json:"attributes"`
+		Events      map[string]int    `json:"events"`
+		LastUpdated int               `json:"last_updated"`
+	}
+	var data = []*Customer{}
+
+	var jsonFile, err = ioutil.ReadFile(jsonFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var stream string = string(jsonFile)
+	var dd = json.NewDecoder(strings.NewReader(stream))
+
+	for { // for each  new line
+		var err = dd.Decode(&data)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			break
+		}
+	}
 
 	router.GET("/customers", func(c *gin.Context) {
-		c.String(http.StatusOK, "{customers: 0}")
+
+		// sort by descending user id
+		sort.SliceStable(data, func(i, j int) bool {
+			return data[i].ID > data[j].ID
+		})
+
+		output, _ := json.Marshal(data)
+		s := string(output)
+
+		c.String(http.StatusOK, s)
 	})
 
 	router.GET("/customers/:uid", func(c *gin.Context) {
-		uid := c.Param("uid")
-		c.String(http.StatusOK, "Hello"+uid)
+		str_uid := c.Param("uid")
+		uid, err := strconv.Atoi(str_uid)
+
+		if err == nil {
+			for i := range data {
+				if data[i].ID == uid {
+					output, _ := json.Marshal(data[i])
+					s := string(output)
+
+					c.String(http.StatusOK, s)
+				}
+			}
+		}
+
 	})
 
 	router.Run(":" + port)
