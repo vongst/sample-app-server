bed810170caac11b556edb980fb403569c51a299
vongst <st@vong.io>
2021-08-30 10:15:33 +0800

    refactored summarize.py

--- summarize.py
+++ summarize.py
@@ -8,10 +8,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("file", help="path to input ndjson file")
 args = parser.parse_args()
 
-# load from file-like objects
-with open(args.file) as f:
-    data = ndjson.load(f)
-
 def write_to_json(content=""):
     with open(args.file + ".out", 'w', encoding='utf-8') as f:
         json.dump(content, f, ensure_ascii=False, indent=4)
@@ -29,49 +25,41 @@ def split_attributes_and_events(data: list) -> tuple:
 
     return attr, events
 
-def get_events_dict(events_arr: list) -> dict: 
+def format_events(events_arr: list) -> dict: 
     events = defaultdict(dict)
 
     for e in events_arr: 
         uid = int(e["user_id"])
-        
-        if e["name"] not in events[uid]:
-            events[uid][e["name"]] = 1
+        if uid not in events:
+            ev = events[uid]["events"] = defaultdict(dict)
+        if e["name"] not in ev:
+            ev[e["name"]] = 1
         else:
-            events[uid][e["name"]] += 1 
+            ev[e["name"]] += 1 
 
     return events
 
-def get_attributes_dict(attr_arr: list) -> dict: 
+def format_attr(attr_arr: list) -> dict: 
     attr = defaultdict(dict)
 
-    for item in attr_arr: 
-        uid = int(item["user_id"])
+    for a in attr_arr: 
+        uid = int(a["user_id"])
 
         if "attributes" not in attr[uid]:
-            attr[uid]["attributes"] = item["data"]
-            attr[uid]["timestamps"] = [item["timestamp"]]
+            attr[uid]["attributes"] = a["data"]
+            attr[uid]["timestamps"] = [a["timestamp"]]
         elif "attributes" in attr[uid]:
-            attr[uid]["attributes"].update(item["data"])
-            attr[uid]["timestamps"].append(item["timestamp"])
+            attr[uid]["attributes"].update(a["data"])
+            attr[uid]["timestamps"].append(a["timestamp"])
 
     return attr
 
-def get_formatted(attr: dict, events: dict) -> dict:
-    customers = []
+with open(args.file) as f:
+    data = ndjson.load(f)
 
-    for uid in events: 
-        attr[uid]["events"] = events[uid]
-        attr[uid]["id"] = int(uid)
-        attr[uid]["last_updated"] = int(attr[uid]["attributes"]["created_at"])
+attr, events = split_attributes_and_events(data)
+attr    = format_attr(attr)
+events  = format_events(events)
 
-        customers.append(attr[uid])
-    
-    return customers
-
-attributes, events = split_attributes_and_events(data)
-
-events = get_events_dict(events)
-attr = get_attributes_dict(attributes)
-formatted = get_formatted(attr, events)
-write_to_json(formatted)
+# merge events dict onto attributes dict
+write_to_json([{**events, **attr}])
